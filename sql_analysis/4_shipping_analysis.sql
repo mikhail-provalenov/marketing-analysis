c3b88ae2f848dde290aa1d50579eef61d453450f
mikhail-provalenov <mishkabarsu@gmail.com>
2025-11-11 19:29:56 +0300

    Update and rename 4_recommendations.sql to 4_shipping_analysis.sql

---
+++ sql_analysis/4_shipping_analysis.sql
@@ -0,0 +1,12 @@
+-- 3. АНАЛИЗ ЭФФЕКТИВНОСТИ ДОСТАВКИ
+SELECT 
+    "Ship Mode",
+    COUNT(*) as total_orders,
+    ROUND(SUM(Sales), 2) as total_sales,
+    ROUND(SUM(Profit), 2) as total_profit,
+    ROUND(AVG(JULIANDAY("Ship Date") - JULIANDAY("Order Date")), 1) as avg_delivery_days,
+    ROUND(AVG(Discount) * 100, 1) as avg_discount_percent,
+    ROUND(SUM(Profit) / COUNT(*), 2) as profit_per_order
+FROM superstore
+GROUP BY "Ship Mode"
+ORDER BY profit_per_order DESC;
