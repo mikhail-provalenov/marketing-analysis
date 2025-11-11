4b75a73f3282152e68220712df0c0b49dada443c
mikhail-provalenov <mishkabarsu@gmail.com>
2025-11-11 17:34:35 +0300

    Update 1_basic_metrics.sql

--- sql_analysis/1_basic_metrics.sql
+++ sql_analysis/1_basic_metrics.sql
@@ -1,1 +1,7 @@
-
+SELECT 
+    COUNT(DISTINCT Order_ID) as total_orders,
+    ROUND(SUM(Sales), 2) as total_revenue,
+    ROUND(SUM(Profit), 2) as total_profit,
+    COUNT(DISTINCT Customer_ID) as unique_customers,
+    COUNT(DISTINCT Product_ID) as unique_products
+FROM superstore_sales_table;
